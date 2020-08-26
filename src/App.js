8c58275e5df2cd6eb118f14f884c17e236622529
Rafael Ferreira dos Santos <osmarditto@gmail.com>
2020-08-26 14:19:06 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Formulario from './components/Formulario/Formulario';
+import { Typography, Container } from '@material-ui/core';
+// import 'fontsource-roboto'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="article" maxWidth="md">
+      <Typography variant="h3" component="h1" color="primary" align="center">Formulário de Cadastro</Typography>
+      <Formulario callBack={GetFormData} validateCPF={ValidaCPF} />
+    </Container>
   );
 }
+function GetFormData(data) {
+  console.log(data);
+}
+function ValidaCPF(cpf) {
+  console.log("Validando CPF");
+  if (cpf.length !== 11) {
 
+    return { valido: false, msg: "O CPF deve possuir 11 dígitos." }
+  }
+  return { valido: true, msg: "CPF válido." }
+}
 export default App;
